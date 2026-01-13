6106062c2d39cac0939abe88e0110d00d18324a6
Devansh Bhatt <devanshbhatt@Devanshs-MacBook-Air.local>
2026-01-13 12:11:13 -0500

    Defined state and config files

---
+++ backend/agent/state.py
@@ -0,0 +1,3 @@
+from typing import TypedDict, Annotated, Sequence
+from langgraph.core import BaseMessage
+from langgraph.graph.message import add_message
---
+++ backend/config.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    """Application settings initialization and loading from environment file"""
+
+    #LLM Configuration
+    groq_api_key:str
+    llm_model:str = "llama-3.3-70b-versatile"
+    llm_temperature:float = 0.7
+
+    # Agent Configuration
+    max_iterations: int = 10 # max retries
+    
+    # API Configuration
+    api_title: str = "AI Research Assistant"
+    api_version: str = "1.0.0"
+    
+    # # Optional: Database
+    # database_url: str = "sqlite:///./research_assistant.db"
+    
+    class Config:
+        env_file = ".env"
+        case_sensitive = False
+
+@lru_cache()
+def get_settings() -> Settings:
+    """Get cached settings instance."""
+    return Settings()
+
+
